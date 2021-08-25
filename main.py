f5110528a8c2d22a48edadbbbd8062ed7a839c2e
コンソメ <icebarrier2700@gmail.com>
2021-08-25 09:05:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ list_for_dyno = list_for_dyno[1:]
 
  # print(list_for_dyno)
 
-readme_md = "単体禁止ワード " + str(default_banned_words) + "\n" + "疑惑名詞 " + str(nouns) + "\n" + "疑惑修飾 " + str(adjs) + "\n\n" "Dyno向け禁止ワードリスト\n" + "> " + list_for_dyno
+readme_md = "単体禁止ワード\n>" + str(default_banned_words) + "疑惑名詞\n>" + str(nouns) + "疑惑修飾\n>" + str(adjs) + "Dyno向け禁止ワードリスト\n>" + list_for_dyno
 
 
 with open("README.MD","w") as text_file:
