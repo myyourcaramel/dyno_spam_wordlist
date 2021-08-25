bdd3e3488a1b38eff58a4e46d6437e993f8bda64
コンソメ <icebarrier2700@gmail.com>
2021-08-24 22:13:22 -0400

    Create main.py
    ban list for dyno automod

---
+++ main.py
@@ -0,0 +1,18 @@
+default_banned_words = ["@everyone", "@here", "awordfordynoautomodtest", "CSGO", "CS:GO"]
+
+nouns = ["nitro", "discord nitro", "gift", "giveaway", "airdrop", "case"]
+adjs = ["free", "for free", "giving out", "giveaway", "distribution"]
+
+banned_words = default_banned_words
+for noun in nouns:
+    for adj in adjs:
+        banned_word = noun + " " + adj
+        banned_words.append(banned_word)
+        banned_word = adj + " " + noun
+        banned_words.append(banned_word)
+
+list_for_dyno = ""
+for banned_word in banned_words:
+    list_for_dyno = list_for_dyno + "," + banned_word
+
+print(list_for_dyno)
